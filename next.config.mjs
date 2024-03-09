246648ecc806ba7846f64b9c36de7e8c3a380afa
Oleksandr Perlov <oleksandrperlov@gmail.com>
2024-03-09 19:03:08 +0200

    configure next

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  //remove it later
+  images: { unoptimized: true },
+
   compiler: {
     styledComponents: true,
   },
